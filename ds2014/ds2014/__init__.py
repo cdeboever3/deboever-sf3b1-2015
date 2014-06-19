dff5f1a447995272ac9a67045cc59864ffa102b8
Chris DeBoever <cdeboever3@gmail.com>
2014-06-18 18:55:57 -0700

    Added more files etc.

--- ds2014/ds2014/__init__.py
+++ ds2014/ds2014/__init__.py
@@ -1,4 +1,5 @@
 import os
+from os.path import join
 
 import matplotlib.colors as colors
 import matplotlib.ticker as tkr
@@ -8,70 +9,92 @@ import pandas as pd
 # Rather than specifying paths all over, I'll just put the paths to commonly
 # used files and directories here and use the package to open them. 
 x = os.getcwd()
-root = os.path.join(x.split('deboever-sf3b1-2014')[0], 'deboever-sf3b1-2014')
+root = join(x.split('deboever-sf3b1-2014')[0], 'deboever-sf3b1-2014')
 
 ## data
-subdir = 'data'
-metadata = os.path.join(root, subdir, 'metadata.tsv')
-cosmic_sf3b1 = os.path.join(root, subdir, 'cosmic_sf3b1.tsv')
-sf3b1_mut_info = os.path.join(root, subdir, 'sf3b1_mut_info.tsv')
-expressdir = os.path.join(root, subdir, 'express')
-sjoutdir = os.path.join(root, subdir, 'sjout')
-logfinaldir = os.path.join(root, subdir, 'logfinal')
+subdir = join(root, 'data')
+metadata = join(subdir, 'metadata.tsv')
+cosmic_sf3b1 = join(subdir, 'cosmic_sf3b1.tsv')
+sf3b1_mut_info = join(subdir, 'sf3b1_mut_info.tsv')
+expressdir = join(subdir, 'express')
+sjoutdir = join(subdir, 'sjout')
+logfinaldir = join(subdir, 'logfinal')
 
 ## ext_data
-subdir = 'ext_data'
-gencode_gtf = os.path.join(root, subdir, 'gencode',
+subdir = join(root, 'ext_data')
+gencode_gtf = join(subdir, 'gencode',
                            'gencode.v14.annotation.gtf')
-gencode_transcripts = os.path.join(root, subdir, 'gencode',
+gencode_transcripts = join(subdir, 'gencode',
                                    'gencode.v14.pc_transcripts.fa')
-gencode_translations = os.path.join(root, subdir, 'gencode',
+gencode_translations = join(subdir, 'gencode',
                                    'gencode.v14.pc_translations.fa')
+hg19 = join(subdir, 'hg19.fa')
 
 ### gencode_sjout_express_processing
-subdir = os.path.join('output', 'gencode_sjout_express_processing')
-gene_info = os.path.join(root, subdir, 'gene_info.tsv')
-transcript_eff_counts = os.path.join(root, subdir, 'transcript_eff_counts.tsv')
-gene_eff_counts = os.path.join(root, subdir, 'gene_eff_counts.tsv')
-junction_info = os.path.join(root, subdir, 'junction_info.tsv')
-transcripts_genes = os.path.join(root, subdir, 'transcripts_genes.tsv')
-star_logs = os.path.join(root, subdir, 'star_logs.tsv')
-um_sj_counts = os.path.join(root, subdir, 'um_sj_counts.tsv')
-um_sj_annot = os.path.join(root, subdir, 'um_sj_annot.tsv')
-um_sj_stats = os.path.join(root, subdir, 'um_sj_stats.tsv')
-cll_sj_counts = os.path.join(root, subdir, 'cll_sj_counts.tsv')
-cll_sj_annot = os.path.join(root, subdir, 'cll_sj_annot.tsv')
-cll_sj_stats = os.path.join(root, subdir, 'cll_sj_stats.tsv')
-brca_sj_counts = os.path.join(root, subdir, 'brca_sj_counts.tsv')
-brca_sj_annot = os.path.join(root, subdir, 'brca_sj_annot.tsv')
-brca_sj_stats = os.path.join(root, subdir, 'brca_sj_stats.tsv')
-brca_cll_um_sj_counts = os.path.join(root, subdir, 'brca_cll_um_sj_counts.tsv')
-brca_cll_um_sj_annot = os.path.join(root, subdir, 'brca_cll_um_sj_annot.tsv')
-brca_cll_um_sj_stats = os.path.join(root, subdir, 'brca_cll_um_sj_stats.tsv')
-brca_cll_luad_lusc_um_sj_counts = os.path.join(root, subdir, 
-                                               'brca_cll_luad_lusc_um_sj_counts.tsv')
-brca_cll_luad_lusc_um_sj_annot = os.path.join(root, subdir, 
-                                              'brca_cll_luad_lusc_um_sj_annot.tsv')
-brca_cll_luad_lusc_um_sj_stats = os.path.join(root, subdir, 
-                                              'brca_cll_luad_lusc_um_sj_stats.tsv')
+subdir = join(root, 'output', 'gencode_sjout_express_processing')
+gene_info = join(subdir, 'gene_info.tsv')
+transcript_eff_counts = join(subdir, 'transcript_eff_counts.tsv')
+gene_eff_counts = join(subdir, 'gene_eff_counts.tsv')
+junction_info = join(subdir, 'junction_info.tsv')
+transcripts_genes = join(subdir, 'transcripts_genes.tsv')
+star_logs = join(subdir, 'star_logs.tsv')
+um_sj_counts = join(subdir, 'um_sj_counts.tsv')
+um_sj_annot = join(subdir, 'um_sj_annot.tsv')
+um_sj_stats = join(subdir, 'um_sj_stats.tsv')
+cll_sj_counts = join(subdir, 'cll_sj_counts.tsv')
+cll_sj_annot = join(subdir, 'cll_sj_annot.tsv')
+cll_sj_stats = join(subdir, 'cll_sj_stats.tsv')
+brca_sj_counts = join(subdir, 'brca_sj_counts.tsv')
+brca_sj_annot = join(subdir, 'brca_sj_annot.tsv')
+brca_sj_stats = join(subdir, 'brca_sj_stats.tsv')
+brca_cll_um_sj_counts = join(subdir, 'brca_cll_um_sj_counts.tsv')
+brca_cll_um_sj_annot = join(subdir, 'brca_cll_um_sj_annot.tsv')
+brca_cll_um_sj_stats = join(subdir, 'brca_cll_um_sj_stats.tsv')
+brca_cll_luad_lusc_um_sj_counts = join(subdir, 'brca_cll_luad_lusc_um_sj_counts.tsv')
+brca_cll_luad_lusc_um_sj_annot = join(subdir, 'brca_cll_luad_lusc_um_sj_annot.tsv')
+brca_cll_luad_lusc_um_sj_stats = join(subdir, 'brca_cll_luad_lusc_um_sj_stats.tsv')
 
 ### dexseq
-subdir = os.path.join('output', 'dexseq')
-brca_dexseq_results = os.path.join(root, subdir, 'brca_dexseq_results.tsv')
-cll_dexseq_results = os.path.join(root, subdir, 'cll_dexseq_results.tsv')
-um_dexseq_results = os.path.join(root, subdir, 'um_dexseq_results.tsv')
-brca_cll_um_dexseq_results = os.path.join(root, subdir,
-                                          'brca_cll_um_dexseq_results.tsv')
-brca_cll_luad_lusc_um_dexseq_results = os.path.join(
-    root, subdir, 'brca_cll_luad_lusc_um_dexseq_results.tsv')
+subdir = join(root, 'output', 'dexseq')
+brca_dexseq_results = join(subdir, 'brca_dexseq_results.tsv')
+cll_dexseq_results = join(subdir, 'cll_dexseq_results.tsv')
+um_dexseq_results = join(subdir, 'um_dexseq_results.tsv')
+brca_cll_um_dexseq_results = join(subdir, 'brca_cll_um_dexseq_results.tsv')
+brca_cll_luad_lusc_um_dexseq_results = join(subdir, 'brca_cll_luad_lusc_um_dexseq_results.tsv')
+brca_size_factors = join(subdir, 'brca_size_factors.tsv')
+cll_size_factors = join(subdir, 'cll_size_factors.tsv')
+um_size_factors = join(subdir, 'um_size_factors.tsv')
+brca_cll_um_size_factors = join(subdir, 'brca_cll_um_size_factors.tsv')
+brca_cll_luad_lusc_um_size_factors = join(subdir, 'brca_cll_luad_lusc_um_size_factors.tsv')
 
-brca_size_factors = os.path.join(root, subdir, 'brca_size_factors.tsv')
-cll_size_factors = os.path.join(root, subdir, 'cll_size_factors.tsv')
-um_size_factors = os.path.join(root, subdir, 'um_size_factors.tsv')
-brca_cll_um_size_factors = os.path.join(root, subdir,
-                                        'brca_cll_um_size_factors.tsv')
-brca_cll_luad_lusc_um_size_factors = os.path.join(
-    root, subdir, 'brca_cll_luad_lusc_um_size_factors.tsv')
+### branchpoint_analysis
+subdir = join(root, 'output', 'branchpoint_analysis')
+proximal_jxns = join(subdir, 'proximal_jxns.tsv')
+distal_jxns = join(subdir, 'distal_jxns.tsv')
+annot_jxns = join(subdir, 'annot_jxns.tsv')
+control_jxns = join(subdir, 'control_jxns.tsv')
+
+proximal_to_annot = join(subdir, 'proximal_to_annot.tsv')
+
+proximal_intron_seq = join(subdir, 'proximal_intron_seq.tsv')
+distal_intron_seq = join(subdir, 'distal_intron_seq.tsv')
+annot_intron_seq = join(subdir, 'annot_intron_seq.tsv')
+control_intron_seq = join(subdir, 'control_intron_seq.tsv')
+
+proximal_all_bp = join(subdir, 'proximal_all_bp.tsv')
+distal_all_bp = join(subdir, 'distal_all_bp.tsv')
+annot_all_bp = join(subdir, 'annot_all_bp.tsv')
+control_all_bp = join(subdir, 'control_all_bp.tsv')
+
+proximal_single_bp = join(subdir, 'proximal_single_bp.tsv')
+distal_single_bp = join(subdir, 'distal_single_bp.tsv')
+annot_single_bp = join(subdir, 'annot_single_bp.tsv')
+control_single_bp = join(subdir, 'control_single_bp.tsv')
+
+proximal_single_bp_second = join(subdir, 'proximal_single_bp_second.tsv')
+distal_single_bp_second = join(subdir, 'distal_single_bp_second.tsv')
+annot_single_bp_second = join(subdir, 'annot_single_bp_second.tsv')
+control_single_bp_second = join(subdir, 'control_single_bp_second.tsv')
 
 ## Other stuff
 dexseq_p_cutoff = 0.1
